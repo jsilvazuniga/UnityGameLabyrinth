9ce4327d55b453352a62adcacfd751a1f3349da5
JORGE RICARDO SILVA ZUÑIGA <JSilvaZuniga@hotmail.com>
2020-07-30 15:49:54 +0200

    Création d'animation d'ouverture port
    création de porte
    création d'animation d'ouverture

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log(other.name + " est à l'interieur du trigger");
+        }
+    }
+
+
+}
