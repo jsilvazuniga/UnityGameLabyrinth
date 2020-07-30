2923fd69cb94920ba28a671e4c61f33687fd7af8
JORGE RICARDO SILVA ZUÑIGA <JSilvaZuniga@hotmail.com>
2020-07-30 16:37:03 +0200

    Overturede porte et doorlight

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -9,7 +9,8 @@ public class DoorScript : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log(other.name + " est à l'interieur du trigger");
+            //   Debug.Log(other.name + " est à l'interieur du trigger");
+            GetComponent<Animator>().enabled = true;
         }
     }
 
