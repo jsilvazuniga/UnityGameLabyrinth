320f61da7771784c8369075084629a771a65d79c
JORGE RICARDO SILVA ZUÑIGA <JSilvaZuniga@hotmail.com>
2020-08-07 14:53:47 +0200

    creation de courtime

---
+++ Assets/Scripts/CountDownScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownScript : MonoBehaviour
+{
+    [SerializeField]
+    private int startCountDown = 60;
+
+    [SerializeField]
+    Text TxtTimeLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TxtTimeLeft.text = "TimeLeft: " + startCountDown;
+        StartCoroutine(Pause());
+    }
+
+    IEnumerator Pause()
+    {
+        while (startCountDown > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            startCountDown--;
+            TxtTimeLeft.text = "TimeLeft: " + startCountDown;
+        }
+
+        Debug.Log("You ar died!!!");
+    }
+
+}
