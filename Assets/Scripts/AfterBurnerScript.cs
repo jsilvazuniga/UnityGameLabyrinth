c7532ed849e97bae62c366a8fa0c0c0ccc7e35ce
JORGE RICARDO SILVA ZUÑIGA <JSilvaZuniga@hotmail.com>
2020-08-09 14:44:19 +0200

    Creation level 2

--- Assets/Scripts/AfterBurnerScript.cs
+++ Assets/Scripts/AfterBurnerScript.cs
@@ -1,15 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AfterBurnerScript : MonoBehaviour
 {
+
+    [SerializeField]
+    int LevelToLoad;
+
+    [SerializeField]
+    bool autoIndex = true;
+
+    private void Start()
+    {
+        if (autoIndex)
+        {
+            LevelToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.tag == "Player")
         {
-            Debug.Log("win");
+           // Debug.Log("win");
+            //recupera el indice de la scena y va a la siguiente
+            SceneManager.LoadScene(LevelToLoad);
         }
     }
 
