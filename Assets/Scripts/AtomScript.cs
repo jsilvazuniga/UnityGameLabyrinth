186f16e418669f28d36f2e4e874776aa235c201a
JORGE RICARDO SILVA ZUÑIGA <JSilvaZuniga@hotmail.com>
2020-08-10 16:13:10 +0200

    creation de aton enemy con script

---
+++ Assets/Scripts/AtomScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtomScript : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 4f;
+
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.collider.tag);
+
+        if (collision.collider.tag == "Player")
+        {
+            GameObject.Find("Player").GetComponent<PlayerController>().GameOver();
+        }
+    }
+}
---
+++ Assets/Scripts/ReturnScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnScript : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform myTarget;
+
+    [SerializeField]
+    AtomScript atomScript;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Ball")
+        {
+            atomScript.target = myTarget;
+
+        }
+    }
+
+
+}
