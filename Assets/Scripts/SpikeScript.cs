e8fbb8bcbd27a07905aa128f514d582a4478d420
JORGE RICARDO SILVA ZUÑIGA <JSilvaZuniga@hotmail.com>
2020-08-10 12:43:40 +0200

    Creation de un enemy

---
+++ Assets/Scripts/SpikeScript.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeScript : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.collider.tag );
+
+        if(collision.collider.tag == "Player")
+        {
+            GameObject.Find("Player").GetComponent<PlayerController>().GameOver();
+        }
+    }
+
+}
